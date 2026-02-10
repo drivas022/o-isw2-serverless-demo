8818487ad1e6f097e9540732a84ca553416a3b03
drivas022 <115203434+drivas022@users.noreply.github.com>
2026-02-09 19:20:32 -0600

    Simular falla Reto 5 /api/procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -1,6 +1,12 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
+  if (String(nombre).toLowerCase() === "error") {
+    return res.status(500).json({
+      error: "Fallo simulado en /api/procesar"
+    });
+  }
+
   res.status(200).json({
     resultado: `Nombre procesado: ${String(nombre).toUpperCase()}`,
     longitud: String(nombre).length
